32bb236d79944029da47eb2d18367bc3546fe891
danillo <danillomello.developer@gmail.com>
2023-03-08 22:14:56 -0300

    TESTE

--- ifood/cadastro/src/main/java/resources/ExerciseResource.java
+++ ifood/cadastro/src/main/java/resources/ExerciseResource.java
@@ -9,6 +9,7 @@ import javax.inject.Inject;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import java.math.BigDecimal;
 import java.util.List;
 
 @Path("/exercises")
@@ -19,6 +20,7 @@ public class ExerciseResource {
     ExerciseService exerciseService;
     @GET
     public List<Exercise> listAll() {
+        BigDecimal teste = BigDecimal.ONE.divide(BigDecimal.ZERO);
         return exerciseService.listAll();
     }
     @POST
